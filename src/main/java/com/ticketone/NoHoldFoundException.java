7b506f076e0a29a13dfb024eee3e6cdea2d29cfa
cindyLi007 <findingnemo2000@gmail.com>
2016-02-03 20:48:13 -0500

    Delete NoHoldFoundException.java

--- src/main/java/com/ticketone/NoHoldFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ticketone;
-
-public class NoHoldFoundException extends RuntimeException {
-  public NoHoldFoundException(int seatHoldId) {
-    super("There is no hold " +  seatHoldId + " exists!");
-  }
-}
