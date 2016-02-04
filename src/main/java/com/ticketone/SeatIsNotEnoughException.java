5c14e90b4d14068fc05a71fffd52c455cc202834
cindyLi007 <findingnemo2000@gmail.com>
2016-02-03 20:48:21 -0500

    Delete SeatIsNotEnoughException.java

--- src/main/java/com/ticketone/SeatIsNotEnoughException.java
+++
@@ -1,7 +0,0 @@
-package com.ticketone;
-
-public class SeatIsNotEnoughException extends RuntimeException {
-  public SeatIsNotEnoughException(int numSeats) {
-    super("There is no enough seats to hold for: " +  numSeats + " seats");
-  }
-}
